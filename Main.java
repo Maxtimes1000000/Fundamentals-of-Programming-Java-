550b5e3ea40d80ea2690ebe509dcf76e23bda3d2
Max Senisch <59898989+Maxtimes1000000@users.noreply.github.com>
2021-12-13 21:57:43 +0000

    Chapter 1 complete

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 /*
-Start Date: 12/12/2021          Latest Change Date: 12/13/2021
+Start Date: 12/12/2021          Latest Change Date: 12/13/2021          IDE:repl.it also known as replit.com
 Howdy. My name is Max S. and this program is just me going through and studying the Intoduction to JAVA Programming and Data Structures Comprehensive Version by Y. Daniel Liang 12th Edition
 This is also going to be my practice with being consistent about commiting things to GitHub
 
-Chapter 1: lines 25-47      Chapter 11:                 Chapter 21:
+Chapter 1: lines 26-59      Chapter 11:                 Chapter 21:
 Chapter 2:                  Chapter 12:                 Chapter 22:
 Chapter 3:                  Chapter 13:                 Chapter 23:
 Chapter 4:                  Chapter 14:                 Chapter 24:
@@ -22,6 +22,9 @@ import java.text.DecimalFormat;
 
 class Main {
   public static void main(String[] args) {
+    
+//1------------------------------------------Chapter 1------------------------------------------------//
+
     System.out.println("Hello world!"); //Outputs the words "Hello world" to the console with a new line
     System.out.print("3 * 5 = "); //Outputs "3 * 5 = " to the console without a new line after it
     System.out.println(3 * 5); //Outputs 15 to the console to the right of the previous output of "3 * 5 = "
@@ -45,6 +48,18 @@ class Main {
     System.out.println(" " + ft.format(312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 4)));
     System.out.print("Population 5 years later: ");
     System.out.println(" " + ft.format(312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 5)));
+
+    System.out.println("\n\n\nYou have the following system of equations:\n3.4x + 50.2y = 44.5"); //Chapter 1 Question 1.3 (rule: use no content from later chapters)
+    System.out.println("2.1x + .55y = 5.9\nWhat are the values of x and y");
+    System.out.print("\nx = ");
+    System.out.printf("%.5f" , (((44.5*.55)-(50.2*5.9))/((3.4*.55)-(50.2*2.1))));
+    System.out.print("\ny = ");
+    System.out.printf("%.5f" , (((3.4*5.9)-(44.5*2.1))/((3.4*.55)-(50.2*2.1))));
+    System.out.println(""); //Just messing around with as many options for creating a new line as I can think of
+
+//2------------------------------------------Chapter 2------------------------------------------------//
+
+
 
   }
 }
