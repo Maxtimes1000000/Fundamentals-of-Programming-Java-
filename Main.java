6097d89970cbf575f669aa449f87515c62c9b118
Max Senisch <59898989+Maxtimes1000000@users.noreply.github.com>
2021-12-13 09:35:10 +0000

    Figured out a bit more about output formatting.

--- Main.java
+++ Main.java
@@ -2,31 +2,49 @@
 Start Date: 12/12/2021          Latest Change Date: 12/13/2021
 Howdy. My name is Max S. and this program is just me going through and studying the Intoduction to JAVA Programming and Data Structures Comprehensive Version by Y. Daniel Liang 12th Edition
 This is also going to be my practice with being consistent about commiting things to GitHub
-Chapter 1
+
+Chapter 1: lines 25-47      Chapter 11:                 Chapter 21:
+Chapter 2:                  Chapter 12:                 Chapter 22:
+Chapter 3:                  Chapter 13:                 Chapter 23:
+Chapter 4:                  Chapter 14:                 Chapter 24:
+Chapter 5:                  Chapter 15:
+Chapter 6:                  Chapter 16:
+Chapter 7:                  Chapter 17:
+Chapter 8:                  Chapter 18:
+Chapter 9:                  Chapter 19:
+Chapter 10:                 Chapter 20:
 */
+
+import java.text.DecimalFormat;
+
+
+
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!"); //Outputs the words "Hello world" to the console with a new line
     System.out.print("3 * 5 = "); //Outputs "3 * 5 = " to the console without a new line after it
     System.out.println(3 * 5); //Outputs 15 to the console to the right of the previous output of "3 * 5 = "
     System.out.print("\n\n"); //Outputs two new lines to the console using the escape sequence "\" and the code for a new line
-    System.out.println("    J     A     V     V     A   ");
+    System.out.println("    J     A     V     V     A   "); //Chapter 1 Question 1.3
     System.out.println("    J    A A     V   V     A A  ");
     System.out.println("J   J   AAAAA     V V     AAAAA ");
     System.out.println(" J J   A     A     V     A     A");
 
     System.out.println("\n");
 
-    System.out.println("Current USA population: 312,032,486");
-    System.out.print("Population 1 year later: ");
-    System.out.println((312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365)));
-    System.out.print("Population 2 years later: ");
-    System.out.println((312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 2)));
+    System.out.println("Current USA population: 312,032,486"); //Chapter 1 Question 1.11
+    System.out.printf("Population 1 year later: "); //This line randomly has a printf instead of a print just for fun
+    System.out.printf("%.2f\n" , (312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365))); //This printf is important because "System.out.print() and System.out.println() take a single argument, but printf() may take multiple arguments." According to https://www.geeksforgeeks.org/formatted-output-in-java/
+    System.out.print("Population 2 years later:");
+    DecimalFormat ft = new DecimalFormat("###,###,###.##"); //This is another way of formatting outputs, specifically decimals, but requires that you import java.text.DecimalFormat;
+    System.out.println(" " + ft.format(312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 2)));
     System.out.print("Population 3 years later: ");
-    System.out.println((312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 3)));
+    System.out.println(" " + ft.format(312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 3)));
     System.out.print("Population 4 years later: ");
-    System.out.println((312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 4)));
+    System.out.println(" " + ft.format(312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 4)));
     System.out.print("Population 5 years later: ");
-    System.out.print((312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 5)));
+    System.out.println(" " + ft.format(312032486 + (((1.0 / 7.0) + (1.0 / 45.0) - (1.0 / 13.0)) * 60 * 24 * 365 * 5)));
+
   }
 }
