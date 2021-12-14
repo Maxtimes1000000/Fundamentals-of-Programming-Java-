cd89e85589bfe325202574a796a8f77ef22340e5
Max Senisch <59898989+Maxtimes1000000@users.noreply.github.com>
2021-12-14 05:13:57 +0000

    Working on Chapter 2

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ Start Date: 12/12/2021          Latest Change Date: 12/13/2021          IDE:repl
 Howdy. My name is Max S. and this program is just me going through and studying the Intoduction to JAVA Programming and Data Structures Comprehensive Version by Y. Daniel Liang 12th Edition
 This is also going to be my practice with being consistent about commiting things to GitHub
 
-Chapter 1: lines 26-59      Chapter 11:                 Chapter 21:
-Chapter 2:                  Chapter 12:                 Chapter 22:
+Chapter 1: lines 26-60      Chapter 11:                 Chapter 21:
+Chapter 2: lines 61-106     Chapter 12:                 Chapter 22:
 Chapter 3:                  Chapter 13:                 Chapter 23:
 Chapter 4:                  Chapter 14:                 Chapter 24:
 Chapter 5:                  Chapter 15:
@@ -16,7 +16,8 @@ Chapter 10:                 Chapter 20:
 */
 
 import java.text.DecimalFormat;
-
+import java.util.Scanner; // imports the Scanner class. This type of import is known as a specific import
+import java.util.*; //Imports all of the classes from the package java.util (More detail in Ch:2 pg:38)
 
 
 
@@ -24,7 +25,7 @@ class Main {
   public static void main(String[] args) {
     
 //1------------------------------------------Chapter 1------------------------------------------------//
-
+    /*
     System.out.println("Hello world!"); //Outputs the words "Hello world" to the console with a new line
     System.out.print("3 * 5 = "); //Outputs "3 * 5 = " to the console without a new line after it
     System.out.println(3 * 5); //Outputs 15 to the console to the right of the previous output of "3 * 5 = "
@@ -56,8 +57,50 @@ class Main {
     System.out.print("\ny = ");
     System.out.printf("%.5f" , (((3.4*5.9)-(44.5*2.1))/((3.4*.55)-(50.2*2.1))));
     System.out.println(""); //Just messing around with as many options for creating a new line as I can think of
-
+    */
 //2------------------------------------------Chapter 2------------------------------------------------//
+   
+    double radius; //Declaration of a variable named radius of variable type double
+    double area;
+
+    radius = 20; // "=" is known as the assignment operator because it assigns value to a variable
+    
+    area = 3.14159 * radius * radius; //Assigns the product of pi * r^2 to the area
+
+    System.out.print("\n\nThe are is ");
+    System.out.println(area);
+
+    Scanner input = new Scanner(System.in); //Creates a scanner object
+
+    System.out.print("Enter a number for the radius: ");
+    radius = input.nextDouble();
+
+    area = 3.14159 * radius * radius; //Computes the area but with the new value 
+
+    System.out.print("\nThe Area is: ");
+    System.out.print(area);
+
+    System.out.print("\n\nPlease enter 3 numbers with spaces between each number: ");
+    double num1 = input.nextDouble(); //Creates a new variable "number1" of data type double and asigns the nextDouble that the user inputs to the variable "number1"
+    double num2 = input.nextDouble();
+    double num3 = input.nextDouble();
+
+    double average = (num1+num2+num3)/3;
+
+    System.out.print("\nThe average of the numbers you have given is ");
+    System.out.printf("%.2f" , average); //Print the average to 2 decimals
+
+    int i, j, k=1; //Declares integer variable i, j, & declares and asigns the value 1 to k
+
+    i = j = k = 2; //This is known as a chained assignment. As long as the far right thing has a specified value (variable or not) and the data types are compatiblethen the rest of the variables in the set will be given that value
+
+    System.out.printf("\n\ni = ");
+    System.out.print(i);
+    System.out.printf("\nj = ");
+    System.out.print(j);
+    System.out.printf("\nk = " , k);
+    System.out.print(k);
+
 
 
 
